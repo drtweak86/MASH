dfca73f31e6ecfd284b783d5403d632bd8b1fffd
Jordan <drtweak86@gmail.com>
2026-01-28 02:16:35 +0000

    Allow unused error variants (work in progress)

--- mash-installer/src/errors.rs
+++ mash-installer/src/errors.rs
@@ -5,9 +5,11 @@ pub enum MashError {
     #[error("Missing --yes-i-know flag. This operation is destructive!")]
     MissingYesIKnow,
     
+    #[allow(dead_code)]
     #[error("Operation aborted by user")]
     Aborted,
     
+    #[allow(dead_code)]
     #[error("Command failed: {0}")]
     CommandFailed(String),
 }
